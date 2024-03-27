c40fbbc95e9f0176c7c6410377e4e08d3ba46e99
Sergey Velesko <53015097+raininforest@users.noreply.github.com>
2024-03-27 14:35:47 +0300

    feat(sdds-acore/uikit): View state was added (#12)
    * feat(sdds-acore/uikit): View state was added
    
    * feat(sdds-acore/uikit): Review notes
    
    * feat(sdds-acore/uikit): View state was added

---
+++ sdds-core/uikit/src/main/kotlin/com/sdds/uikit/viewstate/ViewStateHolder.kt
@@ -0,0 +1,13 @@
+package com.sdds.uikit.viewstate
+
+/**
+ * Интерфейс владельца [ViewState]
+ */
+interface ViewStateHolder {
+
+    /**
+     * Состояние
+     * @see [ViewState]
+     */
+    var state: ViewState?
+}
