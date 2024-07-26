f6832e0a822a2412a87a3d3aa9de23e868a0df8b
Sergey Velesko <53015097+raininforest@users.noreply.github.com>
2024-07-26 15:41:45 +0300

    feat(sdds-acore/uikit-compose): sandbox module for view was created (#87)

---
+++ playground/sandbox-compose/build.gradle.kts
@@ -0,0 +1,49 @@
+import com.sdds.plugin.themebuilder.ThemeBuilderMode
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("convention.android-app")
+    id("convention.compose")
+    id(libs.plugins.themebuilder.get().pluginId)
+}
+
+android {
+    namespace = "com.sdds.playground.sandbox"
+
+    defaultConfig {
+        applicationId = "com.sdds.playground.sandbox"
+    }
+}
+
+themeBuilder {
+    themeSource(name = "stylesSalute")
+    compose()
+    ktPackage("com.sdds.playground.sandbox.tokens")
+    mode(ThemeBuilderMode.THEME)
+}
+
+dependencies {
+    implementation("sdds-core:uikit-compose")
+    implementation("sdds-core:icons")
+
+    implementation(libs.base.androidX.core)
+    implementation(libs.base.androidX.appcompat)
+
+    implementation(libs.base.androidX.compose.foundation)
+    implementation(libs.base.androidX.compose.material)
+    implementation(libs.base.androidX.compose.animation)
+    implementation(libs.base.androidX.activity.compose)
+    implementation(libs.base.androidX.lifecycle.compose.viewmodel)
+    implementation(libs.base.koil.compose)
+
+    // Preview support
+    implementation(libs.base.androidX.compose.uiTooling.preview)
+    debugImplementation(libs.base.androidX.compose.uiTooling)
+
+    // UI Tests
+    androidTestImplementation(libs.base.test.ui.compose.jUnit4)
+    debugImplementation(libs.base.test.ui.compose.uiTestManifest)
+
+    // Unit tests
+    testImplementation(libs.base.test.unit.jUnit)
+}
