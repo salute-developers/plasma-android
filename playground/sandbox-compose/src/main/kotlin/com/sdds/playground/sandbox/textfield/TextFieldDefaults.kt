67f4d554181f9ef8ffd04fdbce58d8932b08ddec
Sergey Velesko <53015097+raininforest@users.noreply.github.com>
2024-09-20 12:26:56 +0300

    feat(sdds-acore/uikit-compose): clear text field was implemented (#136)

--- playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/textfield/TextFieldDefaults.kt
+++ playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/textfield/TextFieldDefaults.kt
@@ -31,9 +31,19 @@ internal interface SandboxTextFieldColors {
      * Цвет значения текстового поля
      *
      * @param state состояние текстового поля
+     * @param isClear текстовое поле с фоном или без фона
      */
     @Composable
-    fun valueColor(state: InputState): State<Color>
+    fun valueColor(state: InputState, isClear: Boolean): State<Color>
+
+    /**
+     * Цвет контента в начале
+     *
+     * @param state состояние текстового поля
+     * @param isClear текстовое поле с фоном или без фона
+     */
+    @Composable
+    fun startContentColor(state: InputState, isClear: Boolean): State<Color>
 
     /**
      * Цвет фона текстового поля
@@ -44,12 +54,21 @@ internal interface SandboxTextFieldColors {
     fun backgroundColor(state: InputState): State<Color>
 
     /**
-     * Цвет заглушки текстового поля
+     * Цвет разделителя текстового поля в состоянии без фона
      *
      * @param state состояние текстового поля
      */
     @Composable
-    fun placeholderColor(state: InputState): State<Color>
+    fun dividerColor(state: InputState): State<Color>
+
+    /**
+     * Цвет заглушки текстового поля
+     *
+     * @param state состояние текстового поля
+     * @param isClear текстовое поле с фоном или без фона
+     */
+    @Composable
+    fun placeholderColor(state: InputState, isClear: Boolean): State<Color>
 
     /**
      * Цвет лейбла текстового поля
@@ -150,12 +169,14 @@ internal interface SandboxTextFieldStyles {
      * @param size размер текстового поля
      * @param colors цвета текстового поля
      * @param inputState состояние текстового поля
+     * @param isClear текстовое поле с фоном или без фона
      */
     @Composable
     fun valueStyle(
         size: SandboxTextField.Size,
         colors: SandboxTextFieldColors,
         inputState: InputState,
+        isClear: Boolean,
     ): State<TextStyle>
 
     /**
@@ -181,12 +202,14 @@ internal interface SandboxTextFieldStyles {
      * @param size размер текстового поля
      * @param colors цвета текстового поля
      * @param inputState состояние текстового поля
+     * @param isClear текстовое поле с фоном или без фона
      */
     @Composable
     fun placeholderStyle(
         size: SandboxTextField.Size,
         colors: SandboxTextFieldColors,
         inputState: InputState,
+        isClear: Boolean,
     ): State<TextStyle>
 }
 
@@ -197,12 +220,42 @@ internal interface SandboxTextFieldStyles {
 internal object TextFieldDefaults {
 
     @Composable
-    fun scrollBarConfig(): ScrollBarConfig = ScrollBarConfig(
-        indicatorThickness = 1.dp,
-        indicatorColor = StylesSaluteTheme.colors.surfaceDefaultTransparentTertiary,
-        backgroundColor = StylesSaluteTheme.colors.surfaceDefaultTransparentPrimary,
-        padding = PaddingValues(top = 18.dp, end = 2.dp, bottom = 36.dp),
-    )
+    fun scrollBarConfig(isClear: Boolean): ScrollBarConfig? =
+        if (isClear) {
+            null
+        } else {
+            ScrollBarConfig(
+                indicatorThickness = 1.dp,
+                indicatorColor = StylesSaluteTheme.colors.surfaceDefaultTransparentTertiary,
+                backgroundColor = StylesSaluteTheme.colors.surfaceDefaultTransparentPrimary,
+                padding = PaddingValues(top = 18.dp, end = 2.dp, bottom = 36.dp),
+            )
+        }
+
+    @Composable
+    fun fieldAppearance(
+        isClear: Boolean,
+        colors: SandboxTextFieldColors,
+        inputState: InputState,
+        size: SandboxTextField.Size,
+        hasDivider: Boolean,
+    ): CoreTextField.FieldAppearance {
+        return if (!isClear) {
+            CoreTextField.FieldAppearance.Solid(
+                backgroundColor = colors.backgroundColor(state = inputState).value,
+                shape = textFieldShapeFor(size),
+            )
+        } else {
+            CoreTextField.FieldAppearance.Clear(
+                dividerColor = if (hasDivider) {
+                    colors.dividerColor(inputState).value
+                } else {
+                    Color.Transparent
+                },
+                dividerThickness = 1.dp,
+            )
+        }
+    }
 
     @Composable
     fun SandboxTextField.FieldType.toFieldType(
@@ -211,6 +264,7 @@ internal object TextFieldDefaults {
         hasLabel: Boolean,
         optionalText: String,
         size: SandboxTextField.Size,
+        fieldAppearance: CoreTextField.FieldAppearance,
     ): CoreTextField.FieldType {
         return when (this) {
             SandboxTextField.FieldType.Optional -> CoreTextField.FieldType.Optional(
@@ -218,7 +272,7 @@ internal object TextFieldDefaults {
             )
 
             SandboxTextField.FieldType.Required -> CoreTextField.FieldType.Required(
-                dotBadge = dotBadge(labelPosition, position, hasLabel, size),
+                dotBadge = dotBadge(labelPosition, position, hasLabel, size, fieldAppearance),
             )
         }
     }
@@ -229,6 +283,7 @@ internal object TextFieldDefaults {
         position: CoreTextField.DotBadge.Position,
         hasLabel: Boolean,
         size: SandboxTextField.Size,
+        fieldAppearance: CoreTextField.FieldAppearance,
     ): CoreTextField.DotBadge {
         return when {
             labelPosition == LabelPosition.Outer && hasLabel -> {
@@ -248,6 +303,23 @@ internal object TextFieldDefaults {
                     position = position,
                     horizontalPadding = horizontalPadding,
                     verticalPadding = verticalPadding,
+                )
+            }
+
+            fieldAppearance is CoreTextField.FieldAppearance.Clear -> {
+                CoreTextField.DotBadge(
+                    size = if (size == SandboxTextField.Size.S || size == SandboxTextField.Size.XS) {
+                        6.dp
+                    } else {
+                        8.dp
+                    },
+                    color = StylesSaluteTheme.colors.surfaceDefaultNegative,
+                    position = position,
+                    horizontalPadding = if (size == SandboxTextField.Size.XS) {
+                        4.dp
+                    } else {
+                        6.dp
+                    },
                 )
             }
 
@@ -305,18 +377,19 @@ internal object TextFieldDefaults {
         labelPosition: LabelPosition,
         helperTextPosition: HelperTextPosition,
         singleLine: Boolean,
+        isClear: Boolean,
     ): CoreTextField.Paddings {
         return CoreTextField.Paddings(
-            boxPaddingStart = startContentPadding(size),
-            boxPaddingEnd = endContentPadding(size),
+            boxPaddingStart = startContentPadding(size, isClear),
+            boxPaddingEnd = endContentPadding(size, isClear),
             boxPaddingTop = textTopPadding(size, labelPosition),
-            boxPaddingBottom = textBottomPadding(size, labelPosition, singleLine),
+            boxPaddingBottom = textBottomPadding(size, labelPosition, singleLine, isClear),
             labelPadding = if (labelPosition == LabelPosition.Outer) {
-                outerLabelBottomPadding(size)
+                outerLabelBottomPadding(size, isClear)
             } else {
                 innerLabelToValuePadding(size)
             },
-            helperTextPadding = if (helperTextPosition == HelperTextPosition.Outer) {
+            helperTextPadding = if (helperTextPosition == HelperTextPosition.Outer || isClear) {
                 helperTextTopOuterPadding(size)
             } else {
                 helperTextInnerTopPadding(size)
@@ -352,8 +425,9 @@ internal object TextFieldDefaults {
         size: SandboxTextField.Size,
         labelPosition: LabelPosition,
         singleLine: Boolean,
+        isClear: Boolean,
     ): Dp {
-        return if (singleLine) {
+        return if (singleLine || isClear) {
             singleLineTextTopPadding(size, labelPosition)
         } else {
             when (size) {
@@ -402,20 +476,28 @@ internal object TextFieldDefaults {
             SandboxTextField.Size.XS -> 4.dp
         }
 
-    private fun startContentPadding(size: SandboxTextField.Size): Dp =
-        when (size) {
-            SandboxTextField.Size.L -> 16.dp
-            SandboxTextField.Size.M -> 14.dp
-            SandboxTextField.Size.S -> 12.dp
-            SandboxTextField.Size.XS -> 8.dp
+    private fun startContentPadding(size: SandboxTextField.Size, isClear: Boolean): Dp =
+        if (isClear) {
+            0.dp
+        } else {
+            when (size) {
+                SandboxTextField.Size.L -> 16.dp
+                SandboxTextField.Size.M -> 14.dp
+                SandboxTextField.Size.S -> 12.dp
+                SandboxTextField.Size.XS -> 8.dp
+            }
         }
 
-    private fun endContentPadding(size: SandboxTextField.Size): Dp =
-        when (size) {
-            SandboxTextField.Size.L -> 16.dp
-            SandboxTextField.Size.M -> 14.dp
-            SandboxTextField.Size.S -> 12.dp
-            SandboxTextField.Size.XS -> 8.dp
+    private fun endContentPadding(size: SandboxTextField.Size, isClear: Boolean): Dp =
+        if (isClear) {
+            0.dp
+        } else {
+            when (size) {
+                SandboxTextField.Size.L -> 16.dp
+                SandboxTextField.Size.M -> 14.dp
+                SandboxTextField.Size.S -> 12.dp
+                SandboxTextField.Size.XS -> 8.dp
+            }
         }
 
     private fun innerLabelToValuePadding(size: SandboxTextField.Size): Dp =
@@ -429,12 +511,21 @@ internal object TextFieldDefaults {
             -> 0.dp
         }
 
-    private fun outerLabelBottomPadding(size: SandboxTextField.Size): Dp =
-        when (size) {
-            SandboxTextField.Size.L -> 12.dp
-            SandboxTextField.Size.M -> 10.dp
-            SandboxTextField.Size.S -> 8.dp
-            SandboxTextField.Size.XS -> 6.dp
+    private fun outerLabelBottomPadding(size: SandboxTextField.Size, isClear: Boolean): Dp =
+        if (isClear) {
+            when (size) {
+                SandboxTextField.Size.L -> 4.dp
+                SandboxTextField.Size.M -> 4.dp
+                SandboxTextField.Size.S -> 4.dp
+                SandboxTextField.Size.XS -> 2.dp
+            }
+        } else {
+            when (size) {
+                SandboxTextField.Size.L -> 12.dp
+                SandboxTextField.Size.M -> 10.dp
+                SandboxTextField.Size.S -> 8.dp
+                SandboxTextField.Size.XS -> 6.dp
+            }
         }
 
     private fun helperTextTopOuterPadding(size: SandboxTextField.Size): Dp =
@@ -524,12 +615,34 @@ private class DefaultSandboxTextFieldColors : SandboxTextFieldColors {
     }
 
     @Composable
-    override fun placeholderColor(state: InputState): State<Color> {
+    override fun dividerColor(state: InputState): State<Color> {
+        val color = when (state) {
+            InputState.Normal -> StylesSaluteTheme.colors.surfaceDefaultTransparentTertiary
+            InputState.Focused -> StylesSaluteTheme.colors.surfaceDefaultAccent
+            InputState.Error -> StylesSaluteTheme.colors.surfaceDefaultNegative
+            InputState.Warning -> StylesSaluteTheme.colors.surfaceDefaultWarning
+            InputState.Success -> StylesSaluteTheme.colors.surfaceDefaultPositive
+            InputState.ReadOnly -> StylesSaluteTheme.colors.surfaceDefaultTransparentPrimary
+        }
+        return rememberUpdatedState(color)
+    }
+
+    @Composable
+    override fun placeholderColor(state: InputState, isClear: Boolean): State<Color> {
         return rememberUpdatedState(
             if (state == InputState.Focused) {
                 StylesSaluteTheme.colors.textDefaultTertiary
             } else {
-                StylesSaluteTheme.colors.textDefaultSecondary
+                if (isClear) {
+                    when (state) {
+                        InputState.Error -> StylesSaluteTheme.colors.textDefaultNegative
+                        InputState.Warning -> StylesSaluteTheme.colors.textDefaultWarning
+                        InputState.Success -> StylesSaluteTheme.colors.textDefaultPositive
+                        else -> StylesSaluteTheme.colors.textDefaultSecondary
+                    }
+                } else {
+                    StylesSaluteTheme.colors.textDefaultSecondary
+                }
             },
         )
     }
@@ -544,8 +657,35 @@ private class DefaultSandboxTextFieldColors : SandboxTextFieldColors {
     }
 
     @Composable
-    override fun valueColor(state: InputState): State<Color> {
-        return rememberUpdatedState(textFieldTextColor(state = state))
+    override fun valueColor(state: InputState, isClear: Boolean): State<Color> {
+        return if (isClear) {
+            rememberUpdatedState(
+                when (state) {
+                    InputState.Error -> StylesSaluteTheme.colors.textDefaultNegative
+                    InputState.Warning -> StylesSaluteTheme.colors.textDefaultWarning
+                    InputState.Success -> StylesSaluteTheme.colors.textDefaultPositive
+                    else -> textFieldTextColor(state)
+                },
+            )
+        } else {
+            rememberUpdatedState(textFieldTextColor(state = state))
+        }
+    }
+
+    @Composable
+    override fun startContentColor(state: InputState, isClear: Boolean): State<Color> {
+        return if (isClear) {
+            rememberUpdatedState(
+                when (state) {
+                    InputState.Error -> StylesSaluteTheme.colors.surfaceDefaultNegative
+                    InputState.Warning -> StylesSaluteTheme.colors.surfaceDefaultWarning
+                    InputState.Success -> StylesSaluteTheme.colors.surfaceDefaultPositive
+                    else -> StylesSaluteTheme.colors.textDefaultSecondary
+                },
+            )
+        } else {
+            rememberUpdatedState(StylesSaluteTheme.colors.textDefaultSecondary)
+        }
     }
 
     @Composable
@@ -666,10 +806,11 @@ private class DefaultSandboxTextFieldStyles : SandboxTextFieldStyles {
         size: SandboxTextField.Size,
         colors: SandboxTextFieldColors,
         inputState: InputState,
+        isClear: Boolean,
     ): State<TextStyle> {
         return rememberUpdatedState(
             textFieldTextStyle(size).copy(
-                color = colors.valueColor(inputState).value,
+                color = colors.valueColor(inputState, isClear).value,
             ),
         )
     }
@@ -701,10 +842,11 @@ private class DefaultSandboxTextFieldStyles : SandboxTextFieldStyles {
         size: SandboxTextField.Size,
         colors: SandboxTextFieldColors,
         inputState: InputState,
+        isClear: Boolean,
     ): State<TextStyle> {
         return rememberUpdatedState(
             textFieldTextStyle(size).copy(
-                color = colors.placeholderColor(inputState).value,
+                color = colors.placeholderColor(inputState, isClear).value,
             ),
         )
     }
