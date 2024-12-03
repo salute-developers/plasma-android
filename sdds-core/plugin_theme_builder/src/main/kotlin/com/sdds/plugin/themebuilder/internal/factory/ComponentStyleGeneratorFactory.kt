ef8208e3e5d5b2b4f8de194971f4182cbe8679fa
malilex <alex.malishev270896@gmail.com>
2024-12-03 16:38:16 +0300

    feat(sdds-acore/theme-builder): BasicButton, IconButton and LinkButton size vartiant style generators were implemented.

--- sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/factory/ComponentStyleGeneratorFactory.kt
+++ sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/factory/ComponentStyleGeneratorFactory.kt
@@ -1,12 +1,16 @@
 package com.sdds.plugin.themebuilder.internal.factory
 
 import com.sdds.plugin.themebuilder.DimensionsConfig
+import com.sdds.plugin.themebuilder.ResourcePrefixConfig
 import com.sdds.plugin.themebuilder.internal.PackageResolver
 import com.sdds.plugin.themebuilder.internal.builder.KtFileBuilder
 import com.sdds.plugin.themebuilder.internal.components.button.BasicButtonStyleGeneratorCompose
+import com.sdds.plugin.themebuilder.internal.components.button.BasicButtonStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.components.button.ButtonStyleGeneratorComposeFactory
 import com.sdds.plugin.themebuilder.internal.components.button.IconButtonStyleGeneratorCompose
+import com.sdds.plugin.themebuilder.internal.components.button.IconButtonStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.components.button.LinkButtonStyleGeneratorCompose
+import com.sdds.plugin.themebuilder.internal.components.button.LinkButtonStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.dimens.DimensAggregator
 import com.sdds.plugin.themebuilder.internal.generator.DimenTokenGenerator
 import com.sdds.plugin.themebuilder.internal.utils.ResourceReferenceProvider
@@ -26,6 +30,7 @@ internal class ComponentStyleGeneratorFactory(
     private val dimensionsConfig: DimensionsConfig,
     private val dimensAggregator: DimensAggregator,
     private val resourceReferenceProvider: ResourceReferenceProvider,
+    private val resourcePrefixConfig: ResourcePrefixConfig,
 ) {
     private val outputDir: File by unsafeLazy {
         projectDir.get().dir(outputDirPath).asFile
@@ -51,11 +56,38 @@ internal class ComponentStyleGeneratorFactory(
     fun createBasicButtonStyleGeneratorCompose(): BasicButtonStyleGeneratorCompose =
         buttonStyleGeneratorComposeFactory.createBasicButtonGenerator()
 
+    fun createBasicButtonStyleGeneratorView(): BasicButtonStyleGeneratorView =
+        BasicButtonStyleGeneratorView(
+            xmlBuilderFactory = xmlBuilderFactory,
+            resourceReferenceProvider = resourceReferenceProvider,
+            dimensAggregator = dimensAggregator,
+            outputResDir = outputResDir,
+            attrPrefix = resourcePrefixConfig.resourcePrefix,
+        )
+
     fun createIconButtonStyleGeneratorCompose(): IconButtonStyleGeneratorCompose =
         buttonStyleGeneratorComposeFactory.createIconButtonGenerator()
 
+    fun createIconButtonStyleGeneratorView(): IconButtonStyleGeneratorView =
+        IconButtonStyleGeneratorView(
+            xmlBuilderFactory = xmlBuilderFactory,
+            resourceReferenceProvider = resourceReferenceProvider,
+            dimensAggregator = dimensAggregator,
+            outputResDir = outputResDir,
+            attrPrefix = resourcePrefixConfig.resourcePrefix,
+        )
+
     fun createLinkButtonStyleGeneratorCompose(): LinkButtonStyleGeneratorCompose =
         buttonStyleGeneratorComposeFactory.createLinkButtonGenerator()
+
+    fun createLinkButtonStyleGeneratorView(): LinkButtonStyleGeneratorView =
+        LinkButtonStyleGeneratorView(
+            xmlBuilderFactory = xmlBuilderFactory,
+            resourceReferenceProvider = resourceReferenceProvider,
+            dimensAggregator = dimensAggregator,
+            outputResDir = outputResDir,
+            attrPrefix = resourcePrefixConfig.resourcePrefix,
+        )
 
     fun createDimensionGenerator(): DimenTokenGenerator = DimenTokenGenerator(
         outputResDir = outputResDir,
