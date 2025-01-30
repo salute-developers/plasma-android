e5d337860b112cce05afb194914b2f13eb2dc5c1
malilex <alex.malishev270896@gmail.com>
2025-01-30 12:34:13 +0300

    feat(sdds-acore/uikit): Fallback elevation was implemented to support shadows on api < 28.

--- sdds-core/uikit/src/main/kotlin/com/sdds/uikit/shape/ShadowRenderer.kt
+++ sdds-core/uikit/src/main/kotlin/com/sdds/uikit/shape/ShadowRenderer.kt
@@ -1,13 +1,8 @@
 package com.sdds.uikit.shape
 
-import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.shapes.Shape
-import androidx.annotation.StyleRes
-import androidx.core.content.res.use
 import androidx.core.graphics.withScale
 import com.sdds.uikit.R
 import com.sdds.uikit.internal.base.configure
@@ -20,70 +15,43 @@ import com.sdds.uikit.internal.base.unsafeLazy
  */
 internal class ShadowRenderer {
 
-    private var layers: Array<ShadowLayer>? = null
+    private var shadowLayerRenderers: Array<ShadowLayerRenderer>? = null
 
-    fun setShadowAppearance(context: Context, @StyleRes shadowAppearance: Int) {
-        layers = context.obtainStyledAttributes(shadowAppearance, R.styleable.SdShadowAppearance).use { typedArray ->
-            if (typedArray.hasValue(R.styleable.SdShadowAppearance_sd_shadowLayers)) {
-                typedArray.obtainShadowLayers(context)
-            } else {
-                arrayOf(typedArray.obtainShadowAppearance())
-            }
+    fun setShadowModel(shadowModel: ShadowModel) {
+        this.shadowLayerRenderers = Array(shadowModel.layers.size) {
+            ShadowLayerRenderer(shadowModel.layers[it])
         }
     }
 
     fun render(canvas: Canvas, shape: Shape) {
-        layers?.forEach { it.render(canvas, shape) }
-    }
-
-    private companion object {
-
-        fun TypedArray.obtainShadowAppearance(): ShadowLayer {
-            return ShadowLayer(
-                getDimension(R.styleable.SdShadowAppearance_sd_shadowBlurRadius, 0f),
-                getDimension(R.styleable.SdShadowAppearance_sd_shadowSpreadRadius, 0f),
-                getDimension(R.styleable.SdShadowAppearance_sd_shadowOffsetX, 0f),
-                getDimension(R.styleable.SdShadowAppearance_sd_shadowOffsetY, 0f),
-                getColor(R.styleable.SdShadowAppearance_sd_shadowColor, Color.BLACK),
-            )
-        }
-
-        fun TypedArray.obtainShadowLayers(context: Context): Array<ShadowLayer> {
-            val layersReference = getResourceId(R.styleable.SdShadowAppearance_sd_shadowLayers, 0)
-            return if (layersReference != 0) {
-                resources.obtainTypedArray(layersReference).use { layersTypedArray ->
-                    Array(layersTypedArray.length()) { index ->
-                        ShadowLayer.obtain(context, layersTypedArray.getResourceId(index, 0))
-                    }
-                }
-            } else {
-                emptyArray()
-            }
-        }
+        shadowLayerRenderers?.forEach { it.render(canvas, shape) }
     }
 }
 
-private class ShadowLayer(
-    private val blurRadius: Float = 0f,
-    private val spreadRadius: Float = 0f,
-    private val offsetX: Float = 0f,
-    private val offsetY: Float = 0f,
-    private val color: Int = Color.BLACK,
+private class ShadowLayerRenderer(
+    private val layer: ShadowLayer,
 ) {
 
     private val paint by unsafeLazy {
         Paint()
             .configure(color = 0)
-            .apply { setShadowLayer(blurRadius, offsetX, offsetY, this@ShadowLayer.color) }
+            .apply {
+                setShadowLayer(
+                    layer.blurRadius,
+                    layer.offsetX,
+                    layer.offsetY,
+                    layer.color,
+                )
+            }
     }
 
     fun render(canvas: Canvas, shape: Shape) {
-        if (spreadRadius != 0f) {
+        if (layer.spreadRadius != 0f) {
             val shapeWidth = shape.width
             val shapeHeight = shape.height
             canvas.withScale(
-                spreadScale(spreadRadius, shapeWidth),
-                spreadScale(spreadRadius, shapeHeight),
+                spreadScale(layer.spreadRadius, shapeWidth),
+                spreadScale(layer.spreadRadius, shapeHeight),
                 shapeWidth / 2,
                 shapeHeight / 2,
             ) {
@@ -100,17 +68,5 @@ private class ShadowLayer(
             spread: Float,
             size: Float,
         ): Float = 1.0F + ((spread / size) * 2.0F)
-
-        fun obtain(context: Context, @StyleRes layerAppearance: Int): ShadowLayer {
-            return context.obtainStyledAttributes(layerAppearance, R.styleable.SdShadowLayer).use { typedArray ->
-                ShadowLayer(
-                    typedArray.getDimension(R.styleable.SdShadowLayer_sd_shadowBlurRadius, 0f),
-                    typedArray.getDimension(R.styleable.SdShadowLayer_sd_shadowSpreadRadius, 0f),
-                    typedArray.getDimension(R.styleable.SdShadowLayer_sd_shadowOffsetX, 0f),
-                    typedArray.getDimension(R.styleable.SdShadowLayer_sd_shadowOffsetY, 0f),
-                    typedArray.getColor(R.styleable.SdShadowLayer_sd_shadowColor, Color.BLACK),
-                )
-            }
-        }
     }
 }
