81cd1a85245b5c95f1d8fde0ef56792aac3fcfdd
raininforest <s.velesko@yandex.ru>
2025-02-11 15:48:57 +0300

    feat(sandbox): core module was implemented

---
+++ playground/sandbox-core-integration/src/main/kotlin/com/sdds/playground/sandbox/core/integration/ComposeStyleProvider.kt
@@ -0,0 +1,28 @@
+package com.sdds.playground.sandbox.core.integration
+
+import androidx.compose.runtime.Composable
+import com.sdds.compose.uikit.style.Style
+
+/**
+ * Базовый класс провайдера стилей компонента на Compose
+ */
+abstract class ComposeStyleProvider<K : Any, S : Style> {
+    protected abstract val variations: Map<K, @Composable () -> S>
+
+    /**
+     * Список возможных вариаций компонента
+     */
+    val variants: List<K>
+        get() = variations.keys.toList()
+
+    private val defaultVariant: K
+        get() = variations.entries.first().key
+
+    /**
+     * Вернёт стиль компонента [S] для ключа вариации [key]
+     */
+    @Composable
+    fun style(key: K): S = variations[key]?.invoke()
+        ?: variations[defaultVariant]?.invoke()
+        ?: throw IllegalStateException("Style $key not found")
+}
---
+++ playground/sandbox-core-integration/src/main/kotlin/com/sdds/playground/sandbox/core/integration/ViewStyleProvider.kt
@@ -0,0 +1,45 @@
+package com.sdds.playground.sandbox.core.integration
+
+import androidx.annotation.StyleRes
+import com.sdds.uikit.colorstate.ColorState
+
+/**
+ * Базовый класс провайдера стилей компонента на ViewSystem
+ */
+abstract class ViewStyleProvider<K : Any> {
+    protected abstract val variations: Map<K, Int>
+    protected open val colorVariations: Map<K, ColorState> = emptyMap()
+
+    /**
+     * Список всех возможных вариаций компонента
+     */
+    val variants: List<K>
+        get() = variations.keys.toList()
+
+    /**
+     * Список всех возможных цветовых вариаций компонента
+     */
+    val colorVariants: List<K>
+        get() = colorVariations.keys.toList()
+
+    private val defaultVariant: K
+        get() = variations.entries.first().key
+
+    private val defaultColorVariant: K
+        get() = colorVariations.entries.first().key
+
+    /**
+     * Вернёт ресурс стиля компонента для ключа вариации [key]
+     */
+    @StyleRes
+    fun styleRes(key: K): Int = variations[key]
+        ?: variations[defaultVariant]
+        ?: throw IllegalStateException("Style $key not found")
+
+    /**
+     * Вернёт [ColorState] компонента для ключа вариации [key]
+     */
+    fun colorState(key: K): ColorState = colorVariations[key]
+        ?: colorVariations[defaultColorVariant]
+        ?: throw IllegalStateException("ColorState $key not found")
+}
