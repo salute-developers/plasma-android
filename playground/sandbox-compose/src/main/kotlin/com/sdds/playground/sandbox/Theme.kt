8c9e7136c812729624fdfc1639b284189025c126
raininforest <s.velesko@yandex.ru>
2025-02-11 15:59:59 +0300

    feat(sandbox): theme enum was added

--- playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/Theme.kt
+++ playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/Theme.kt
@@ -1,120 +1,67 @@
 package com.sdds.playground.sandbox
 
-import android.app.Activity
-import android.graphics.Color
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.annotation.StyleRes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
-import com.sdds.compose.uikit.Avatar
-import com.sdds.compose.uikit.AvatarGroup
-import com.sdds.compose.uikit.BasicButton
-import com.sdds.compose.uikit.CheckBox
-import com.sdds.compose.uikit.CheckBoxGroup
-import com.sdds.compose.uikit.Chip
-import com.sdds.compose.uikit.IconButton
-import com.sdds.compose.uikit.LocalAvatarGroupStyle
-import com.sdds.compose.uikit.LocalAvatarStyle
-import com.sdds.compose.uikit.LocalButtonStyle
-import com.sdds.compose.uikit.LocalCheckBoxGroupStyle
-import com.sdds.compose.uikit.LocalCheckBoxStyle
-import com.sdds.compose.uikit.LocalChipStyle
-import com.sdds.compose.uikit.LocalIconButtonStyle
-import com.sdds.compose.uikit.LocalProgressBarStyle
-import com.sdds.compose.uikit.LocalRadioBoxGroupStyle
-import com.sdds.compose.uikit.LocalRadioBoxStyle
-import com.sdds.compose.uikit.LocalSwitchStyle
-import com.sdds.compose.uikit.LocalTextFieldStyle
-import com.sdds.compose.uikit.ProgressBar
-import com.sdds.compose.uikit.RadioBox
-import com.sdds.compose.uikit.RadioBoxGroup
-import com.sdds.compose.uikit.Switch
-import com.sdds.compose.uikit.TextField
-import com.sdds.compose.uikit.internal.focusselector.FocusSelectorMode
-import com.sdds.compose.uikit.internal.focusselector.LocalFocusSelectorMode
-import com.sdds.compose.uikit.style.style
-import com.sdds.serv.styles.avatar.M
-import com.sdds.serv.styles.avatar.S
-import com.sdds.serv.styles.button.basic.Default
-import com.sdds.serv.styles.button.basic.M
-import com.sdds.serv.styles.button.icon.Default
-import com.sdds.serv.styles.button.icon.M
-import com.sdds.serv.styles.checkbox.M
-import com.sdds.serv.styles.checkbox.group.M
-import com.sdds.serv.styles.chip.Default
-import com.sdds.serv.styles.chip.M
-import com.sdds.serv.styles.progress.Default
-import com.sdds.serv.styles.radiobox.M
-import com.sdds.serv.styles.radiobox.group.M
-import com.sdds.serv.styles.switch.M
-import com.sdds.serv.styles.textfield.Default
-import com.sdds.serv.styles.textfield.M
-import com.sdds.serv.styles.textfield.OuterLabel
-import com.sdds.serv.theme.SddsServTheme
-import com.sdds.serv.theme.darkSddsServColors
-import com.sdds.serv.theme.darkSddsServGradients
-import com.sdds.serv.theme.lightSddsServColors
-import com.sdds.serv.theme.lightSddsServGradients
+import com.sdds.playground.sandbox.core.integration.StylesProviderCompose
+import com.sdds.playground.sandbox.core.integration.StylesProviderView
+import com.sdds.playground.sandbox.plasma.sd.service.integration.PlasmaSdServiceComposeStylesProvider
+import com.sdds.playground.sandbox.plasma.sd.service.integration.PlasmaSdServiceThemeWrapper
+import com.sdds.playground.sandbox.plasma.sd.service.integration.PlasmaSdServiceViewStylesProvider
+import com.sdds.playground.sandbox.sdds.serv.integration.SddsServComposeStylesProvider
+import com.sdds.playground.sandbox.sdds.serv.integration.SddsServThemeWrapper
+import com.sdds.playground.sandbox.sdds.serv.integration.SddsServViewStylesProvider
+import com.sdds.playground.sandbox.stylessalute.integration.StylesSaluteComposeStylesProvider
+import com.sdds.playground.sandbox.stylessalute.integration.StylesSaluteThemeWrapper
+import com.sdds.playground.sandbox.stylessalute.integration.StylesSaluteViewStylesProvider
 
-private val DarkColors = darkSddsServColors()
-private val LightColors = lightSddsServColors()
-private val DarkGradients = darkSddsServGradients()
-private val LightGradients = lightSddsServGradients()
-
-/**
- * Тема демо-приложения
- */
-@Composable
-fun SandboxTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit,
+internal enum class Theme(
+    val compose: ThemeInfoCompose,
+    val view: ThemeInfoView,
 ) {
-    val colorScheme = when {
-        darkTheme -> DarkColors
-        else -> LightColors
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = Color.TRANSPARENT
-            window.navigationBarColor = Color.TRANSPARENT
-            window.decorView.setBackgroundColor(colorScheme.backgroundDefaultPrimary.toArgb())
-            WindowCompat.getInsetsController(window, view).apply {
-                isAppearanceLightStatusBars = !darkTheme
-                isAppearanceLightNavigationBars = !darkTheme
-            }
-        }
-    }
+    SddsServ(
+        compose = ThemeInfoCompose(
+            stylesProvider = SddsServComposeStylesProvider,
+            themeWrapper = { SddsServThemeWrapper(it) },
+        ),
+        view = ThemeInfoView(
+            stylesProvider = SddsServViewStylesProvider,
+            themeRes = com.sdds.serv.R.style.Serv_Sdds_MaterialComponents_DayNight,
+        ),
+    ),
+    PlasmaSdService(
+        compose = ThemeInfoCompose(
+            stylesProvider = PlasmaSdServiceComposeStylesProvider,
+            themeWrapper = { PlasmaSdServiceThemeWrapper(it) },
+        ),
+        view = ThemeInfoView(
+            stylesProvider = PlasmaSdServiceViewStylesProvider,
+            themeRes = com.sdds.plasma.sd.service.R.style.Plasma_SdService_MaterialComponents_DayNight,
+        ),
+    ),
+    StylesSalute(
+        compose = ThemeInfoCompose(
+            stylesProvider = StylesSaluteComposeStylesProvider,
+            themeWrapper = { StylesSaluteThemeWrapper(it) },
+        ),
+        view = ThemeInfoView(
+            stylesProvider = StylesSaluteViewStylesProvider,
+            themeRes = com.sdds.stylessalute.R.style.Salute_StylesSalute_MaterialComponents_DayNight,
+        ),
+    ),
+    ;
 
-    SddsServTheme(
-        colors = colorScheme,
-        gradients = if (darkTheme) DarkGradients else LightGradients,
-        content = {
-            CompositionLocalProvider(
-                LocalFocusSelectorMode provides FocusSelectorMode.Border(
-                    borderStroke = BorderStroke(1.dp, colorScheme.surfaceDefaultAccent),
-                    strokePadding = 2.dp,
-                ),
-                LocalSwitchStyle provides Switch.M.style(),
-                LocalButtonStyle provides BasicButton.M.Default.style(),
-                LocalIconButtonStyle provides IconButton.M.Default.style(),
-                LocalCheckBoxStyle provides CheckBox.M.style(),
-                LocalCheckBoxGroupStyle provides CheckBoxGroup.M.style(),
-                LocalRadioBoxStyle provides RadioBox.M.style(),
-                LocalRadioBoxGroupStyle provides RadioBoxGroup.M.style(),
-                LocalProgressBarStyle provides ProgressBar.Default.style(),
-                LocalAvatarStyle provides Avatar.M.style(),
-                LocalAvatarGroupStyle provides AvatarGroup.S.style(),
-                LocalChipStyle provides Chip.M.Default.style(),
-                LocalTextFieldStyle provides TextField.M.OuterLabel.Default.style(),
-                content = content,
-            )
-        },
+    internal class ThemeInfoCompose(
+        val stylesProvider: StylesProviderCompose,
+        val themeWrapper: @Composable (@Composable () -> Unit) -> Unit,
     )
+
+    internal class ThemeInfoView(
+        val stylesProvider: StylesProviderView,
+        @StyleRes val themeRes: Int,
+    )
+
+    internal companion object {
+        val viewDefault = SddsServ.view
+        val composeDefault = SddsServ.compose
+    }
 }
