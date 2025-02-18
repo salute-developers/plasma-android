e364815ce3c579760958adefc2009ccf5cedbeb9
malilex <alex.malishev270896@gmail.com>
2025-02-18 21:36:02 +0300

    fix(sdds-alibs): Avatar styles were fixed.

---
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/common/Background.kt
@@ -0,0 +1,91 @@
+package com.sdds.compose.uikit.internal.common
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.DrawModifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.platform.InspectorValueInfo
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.LayoutDirection
+
+/**
+ * Рисует [brushes] в пределах формы [shape], учитывая прозрачность [alpha]
+ */
+internal fun Modifier.background(
+    brushes: List<Brush>,
+    shape: Shape = RectangleShape,
+    alpha: Float = 1.0f,
+) = this.then(
+    Background(
+        brushes = brushes,
+        alpha = alpha,
+        shape = shape,
+        inspectorInfo = debugInspectorInfo {
+            name = "background"
+            properties["alpha"] = alpha
+            properties["brushes"] = brushes
+            properties["shape"] = shape
+        },
+    ),
+)
+
+private class Background(
+    private val brushes: List<Brush>,
+    private val alpha: Float = 1.0f,
+    private val shape: Shape,
+    inspectorInfo: InspectorInfo.() -> Unit,
+) : DrawModifier, InspectorValueInfo(inspectorInfo) {
+
+    private var lastSize: Size? = null
+    private var lastLayoutDirection: LayoutDirection? = null
+    private var lastOutline: Outline? = null
+
+    override fun ContentDrawScope.draw() {
+        if (shape === RectangleShape) {
+            drawRect()
+        } else {
+            drawOutline()
+        }
+        drawContent()
+    }
+
+    private fun ContentDrawScope.drawRect() {
+        brushes.forEach { drawRect(brush = it, alpha = alpha) }
+    }
+
+    private fun ContentDrawScope.drawOutline() {
+        val outline =
+            if (size == lastSize && layoutDirection == lastLayoutDirection) {
+                lastOutline!!
+            } else {
+                shape.createOutline(size, layoutDirection, this)
+            }
+        brushes.forEach { drawOutline(outline, brush = it, alpha = alpha) }
+        lastOutline = outline
+        lastSize = size
+        lastLayoutDirection = layoutDirection
+    }
+
+    override fun toString(): String =
+        "Background(brushes=$brushes, alpha = $alpha, shape=$shape)"
+
+    override fun equals(other: Any?): Boolean {
+        val otherModifier = other as? Background ?: return false
+        return brushes == otherModifier.brushes &&
+            alpha == otherModifier.alpha &&
+            shape == otherModifier.shape
+    }
+
+    override fun hashCode(): Int {
+        var result = brushes.hashCode()
+        result = 31 * result + alpha.hashCode()
+        result = 31 * result + shape.hashCode()
+        return result
+    }
+}
