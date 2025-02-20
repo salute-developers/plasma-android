e75c4a003ee7865ce26837c1eb2d7688162a7734
raininforest <s.velesko@yandex.ru>
2025-02-20 10:08:15 +0300

    fix(sandbox): cell screen was improved

--- playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/cell/compose/CellScreen.kt
+++ playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/cell/compose/CellScreen.kt
@@ -1,11 +1,19 @@
 package com.sdds.playground.sandbox.cell.compose
 
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sdds.compose.uikit.Avatar
 import com.sdds.compose.uikit.Cell
@@ -13,6 +21,9 @@ import com.sdds.compose.uikit.CheckBox
 import com.sdds.compose.uikit.Icon
 import com.sdds.compose.uikit.RadioBox
 import com.sdds.compose.uikit.Switch
+import com.sdds.compose.uikit.internal.focusselector.LocalFocusSelectorMode
+import com.sdds.compose.uikit.internal.focusselector.applyFocusSelector
+import com.sdds.playground.sandbox.SandboxTheme
 import com.sdds.playground.sandbox.core.compose.ComponentScaffold
 
 @Composable
@@ -23,7 +34,14 @@ internal fun CellScreen() {
 
     ComponentScaffold(
         component = {
+            val interactionSource = remember { MutableInteractionSource() }
+            val isFocused = interactionSource.collectIsFocusedAsState()
             Cell(
+                modifier = Modifier
+                    .focusable(interactionSource = interactionSource)
+                    .applyFocusSelector(
+                        focusSelectorMode = LocalFocusSelectorMode.current,
+                    ) { isFocused.value },
                 style = uiState.cellStyle(),
                 title = AnnotatedString(uiState.title),
                 subtitle = AnnotatedString(uiState.subtitle),
@@ -32,6 +50,7 @@ internal fun CellScreen() {
                 disclosureText = AnnotatedString(uiState.disclosureText),
                 startContent = cellContent(contentType = uiState.startContent),
                 endContent = cellContent(contentType = uiState.endContent),
+                interactionSource = interactionSource,
             )
         },
         propertiesOwner = cellViewModel,
@@ -46,18 +65,39 @@ private fun cellContent(contentType: CellContent): (@Composable RowScope.() -> U
                 CellContent.AVATAR -> Avatar(
                     painter = painterResource(id = com.sdds.icons.R.drawable.ic_scribble_diagonal_24),
                 )
+
                 CellContent.ICON -> Icon(
                     painter = painterResource(id = com.sdds.icons.R.drawable.ic_scribble_diagonal_24),
                     contentDescription = "",
                 )
 
-                CellContent.SWITCH -> Switch(active = true)
-                CellContent.CHECKBOX -> CheckBox(checked = true)
-                CellContent.RADIOBOX -> RadioBox(checked = true)
+                CellContent.SWITCH -> @Composable {
+                    var active by remember { mutableStateOf(false) }
+                    Switch(active = active, onActiveChanged = { active = it })
+                }
+
+                CellContent.CHECKBOX -> @Composable {
+                    var checked by remember { mutableStateOf(false) }
+                    CheckBox(checked = checked, onCheckedChange = { checked = it })
+                }
+
+                CellContent.RADIOBOX -> @Composable {
+                    var checked by remember { mutableStateOf(false) }
+                    RadioBox(checked = checked, onClick = { checked = true })
+                }
+
                 CellContent.NONE -> {}
             }
         }
     } else {
         null
+    }
+}
+
+@Preview
+@Composable
+internal fun CellScreenPreview() {
+    SandboxTheme {
+        CellScreen()
     }
 }
--- sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/cell/BaseCell.kt
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/cell/BaseCell.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -52,6 +53,7 @@ internal fun BaseCell(
                 Column(
                     modifier = Modifier
                         .weight(1f, fill = false)
+                        .fillMaxWidth()
                         .padding(
                             start = style.dimensions.contentPaddingStart,
                             end = style.dimensions.contentPaddingEnd,
