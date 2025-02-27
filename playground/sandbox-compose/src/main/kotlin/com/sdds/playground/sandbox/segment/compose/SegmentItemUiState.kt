5c0883a903be3bd1a42b4c6e99acdf9043a36fb3
raininforest <s.velesko@yandex.ru>
2025-02-27 17:34:24 +0300

    feat(sandbox): SegmentItem screen was implemented

---
+++ playground/sandbox-compose/src/main/kotlin/com/sdds/playground/sandbox/segment/compose/SegmentItemUiState.kt
@@ -0,0 +1,24 @@
+package com.sdds.playground.sandbox.segment.compose
+
+import com.sdds.playground.sandbox.core.compose.UiState
+
+internal data class SegmentItemUiState(
+    override val variant: String = "",
+    val label: String = "Label",
+    val value: String = "",
+    val startContent: SegmentItemContent = SegmentItemContent.NONE,
+    val endContent: SegmentItemContent = SegmentItemContent.NONE,
+    val enabled: Boolean = true,
+    val count: String = "1",
+) : UiState {
+
+    override fun updateVariant(variant: String): UiState {
+        return copy(variant = variant)
+    }
+}
+
+internal enum class SegmentItemContent {
+    NONE,
+    ICON,
+    COUNTER,
+}
