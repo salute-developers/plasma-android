7a0540917f1a3070ac2cb2b23691ce90d9ad7bd5
raininforest <s.velesko@yandex.ru>
2025-02-27 17:32:31 +0300

    feat(sdds-acore/uikit-compose): SegmentItem was implemented

--- sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/common/Modifiers.kt
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/common/Modifiers.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Stable
@@ -30,6 +29,7 @@ import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.sdds.compose.uikit.IndicatorMode
+import com.sdds.compose.uikit.interactions.selectable
 
 /**
  * Модификатор, позволяющий дизэйблить Composable (менять альфу и игнорировать ввод).
@@ -135,13 +135,13 @@ internal fun Modifier.surface(
     interactionSource: MutableInteractionSource,
 ): Modifier {
     val toggleableOrFocusableModifier = onValueChange?.let {
-        Modifier.toggleable(
-            value = value,
+        Modifier.selectable(
+            selected = value,
             interactionSource = interactionSource,
             indication = indication,
             enabled = enabled,
             role = role,
-            onValueChange = onValueChange,
+            onSelectedChanged = onValueChange,
         )
     } ?: Modifier.focusable(enabled, interactionSource)
 
