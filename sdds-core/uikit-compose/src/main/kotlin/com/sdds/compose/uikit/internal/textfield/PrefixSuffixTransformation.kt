1a76d94cff22b3bb4b182785a5d724b10ea04d6a
Sergey Velesko <53015097+raininforest@users.noreply.github.com>
2025-03-04 09:33:34 +0300

    feat(sdds-acore/uikit-compose): prefix and suffix for placeholder were added (#282)

--- sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/textfield/PrefixSuffixTransformation.kt
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/internal/textfield/PrefixSuffixTransformation.kt
@@ -1,5 +1,8 @@
 package com.sdds.compose.uikit.internal.textfield
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.OffsetMapping
@@ -11,20 +14,34 @@ internal class PrefixSuffixTransformation(
     suffix: String = "",
     prefixTextStyle: TextStyle = TextStyle(),
     suffixTextStyle: TextStyle = TextStyle(),
+    transparent: Boolean = false,
 ) : VisualTransformation {
 
     private val prefixWithWhiteSpace = if (prefix.isNotEmpty()) "$prefix " else ""
-    private val suffixWithWhiteSpace = if (suffix.isNotEmpty())" $suffix" else ""
+    private val suffixWithWhiteSpace = if (suffix.isNotEmpty()) " $suffix" else ""
+
+    private val prefixSpanStyle = if (transparent) {
+        prefixTextStyle.copy(color = Color.Transparent)
+    } else {
+        prefixTextStyle
+    }.toSpanStyle()
+
+    private val suffixSpanStyle = if (transparent) {
+        suffixTextStyle.copy(color = Color.Transparent)
+    } else {
+        suffixTextStyle
+    }.toSpanStyle()
 
     private val prefixAnnotated = AnnotatedString(
         text = prefixWithWhiteSpace,
-        spanStyle = prefixTextStyle.toSpanStyle(),
+        spanStyle = prefixSpanStyle,
     )
 
     private val suffixAnnotated = AnnotatedString(
         text = suffixWithWhiteSpace,
-        spanStyle = suffixTextStyle.toSpanStyle(),
+        spanStyle = suffixSpanStyle,
     )
+
     override fun filter(text: AnnotatedString): TransformedText {
         val result = prefixAnnotated + text + suffixAnnotated
 
@@ -41,5 +58,48 @@ internal class PrefixSuffixTransformation(
         }
 
         return TransformedText(result, textWithPrefixSuffixMapping)
+    }
+}
+
+internal fun prefixSuffixTransformation(
+    prefix: String? = null,
+    suffix: String? = null,
+    prefixTextStyle: TextStyle = TextStyle(),
+    suffixTextStyle: TextStyle = TextStyle(),
+    transparent: Boolean = false,
+): VisualTransformation = if (prefix.isNullOrEmpty() && suffix.isNullOrEmpty()) {
+    VisualTransformation.None
+} else {
+    PrefixSuffixTransformation(
+        prefix = prefix.orEmpty(),
+        suffix = suffix.orEmpty(),
+        prefixTextStyle = prefixTextStyle,
+        suffixTextStyle = suffixTextStyle,
+        transparent = transparent,
+    )
+}
+
+@Composable
+internal fun rememberPrefixSuffixTransformation(
+    prefix: String? = null,
+    suffix: String? = null,
+    prefixTextStyle: TextStyle = TextStyle(),
+    suffixTextStyle: TextStyle = TextStyle(),
+    transparent: Boolean = false,
+): VisualTransformation {
+    return remember(
+        prefix,
+        suffix,
+        prefixTextStyle,
+        suffixTextStyle,
+        transparent,
+    ) {
+        prefixSuffixTransformation(
+            prefix = prefix,
+            suffix = suffix,
+            prefixTextStyle = prefixTextStyle,
+            suffixTextStyle = suffixTextStyle,
+            transparent = transparent,
+        )
     }
 }
