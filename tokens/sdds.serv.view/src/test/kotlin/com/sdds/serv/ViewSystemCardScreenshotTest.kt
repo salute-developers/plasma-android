cdf442d8fbc5c76fa3008958c6f6d22dfe1f831d
angirb <bulinoff@gmail.com>
2025-03-19 16:42:32 +0300

    test(sdds-acore/testing): Card screenshot tests

---
+++ tokens/sdds.serv.view/src/test/kotlin/com/sdds/serv/ViewSystemCardScreenshotTest.kt
@@ -0,0 +1,60 @@
+package com.sdds.serv
+
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.sdds.testing.vs.SDK_NUMBER
+import com.sdds.testing.vs.card.CardTestCases
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [SDK_NUMBER], qualifiers = RobolectricDeviceQualifiers.Pixel6)
+class ViewSystemCardScreenshotTest(
+    theme: String,
+) : CardTestCases(theme) {
+
+    @Test
+    override fun testCardSizeLSolidHorizontal() {
+        themedComponent {
+            cardHorizontal(R.style.Serv_Sdds_ComponentOverlays_CardSolidL)
+        }
+    }
+
+    @Test
+    override fun testCardSizeMSolidVertical() {
+        themedComponent {
+            cardVertical(R.style.Serv_Sdds_ComponentOverlays_CardSolidM)
+        }
+    }
+
+    @Test
+    override fun testCardSizeSSolidHorizontal() {
+        themedComponent {
+            cardHorizontal(R.style.Serv_Sdds_ComponentOverlays_CardSolidS)
+        }
+    }
+
+    @Test
+    override fun testCardSizeLClearVertical() {
+        themedComponent {
+            cardVertical(R.style.Serv_Sdds_ComponentOverlays_CardClearL)
+        }
+    }
+
+    @Test
+    override fun testCardSizeMClearHorizontal() {
+        themedComponent {
+            cardHorizontal(R.style.Serv_Sdds_ComponentOverlays_CardClearM)
+        }
+    }
+
+    @Test
+    override fun testCardSizeSClearVertical() {
+        themedComponent {
+            cardVertical(R.style.Serv_Sdds_ComponentOverlays_CardClearS)
+        }
+    }
+}
