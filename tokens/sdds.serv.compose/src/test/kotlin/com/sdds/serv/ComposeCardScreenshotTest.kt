6f169968fad0c48dc80358f209149aeb181f4c62
angirb <119584718+angirb@users.noreply.github.com>
2025-03-20 12:23:19 +0300

    test(sdds-acore/testing): Card compose screenshots (#322)
    * test(sdds-acore/testing): Card compose screenshots
    
    * test(sdds-acore/testing): Compose card fix screenshots

---
+++ tokens/sdds.serv.compose/src/test/kotlin/com/sdds/serv/ComposeCardScreenshotTest.kt
@@ -0,0 +1,68 @@
+package com.sdds.serv
+
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.sdds.compose.uikit.style.style
+import com.sdds.serv.styles.card.CardClear
+import com.sdds.serv.styles.card.CardSolid
+import com.sdds.serv.styles.card.L
+import com.sdds.serv.styles.card.M
+import com.sdds.serv.styles.card.S
+import com.sdds.testing.compose.RoborazziConfigCompose
+import com.sdds.testing.compose.card.CardHorizontal
+import com.sdds.testing.compose.card.CardVertical
+import com.sdds.testing.vs.SDK_NUMBER
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [SDK_NUMBER], qualifiers = RobolectricDeviceQualifiers.Pixel6)
+class ComposeCardScreenshotTest(
+    theme: String,
+) : RoborazziConfigCompose(theme) {
+
+    @Test
+    fun testCardSizeLSolidHorizontal() {
+        composeTestRule.content {
+            CardHorizontal(style = CardSolid.L.style())
+        }
+    }
+
+    @Test
+    fun testCardSizeMSolidVertical() {
+        composeTestRule.content {
+            CardVertical(style = CardSolid.M.style())
+        }
+    }
+
+    @Test
+    fun testCardSizeSSolidHorizontal() {
+        composeTestRule.content {
+            CardHorizontal(style = CardSolid.S.style())
+        }
+    }
+
+    @Test
+    fun testCardSizeLClearVertical() {
+        composeTestRule.content {
+            CardVertical(style = CardClear.L.style())
+        }
+    }
+
+    @Test
+    fun testCardSizeMClearHorizontal() {
+        composeTestRule.content {
+            CardHorizontal(style = CardClear.M.style())
+        }
+    }
+
+    @Test
+    fun testCardSizeSClearVertical() {
+        composeTestRule.content {
+            CardVertical(style = CardClear.S.style())
+        }
+    }
+}
