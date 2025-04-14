56b96ae4815ce61024444cb79548e6691fc3c39e
angirb <bulinoff@gmail.com>
2025-04-14 09:37:19 +0300

    test(sdds-acore/uikit): Avatar screenshot tests compose

--- tokens/plasma.giga.app.compose/src/test/kotlin/com/sdds/plasma/giga/app/ComposeAvatarScreenshotTest.kt
+++ tokens/plasma.giga.app.compose/src/test/kotlin/com/sdds/plasma/giga/app/ComposeAvatarScreenshotTest.kt
@@ -3,14 +3,24 @@ package com.sdds.plasma.giga.app
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.sdds.compose.uikit.style.style
 import com.sdds.plasma.giga.app.styles.avatar.Avatar
+import com.sdds.plasma.giga.app.styles.avatar.L
 import com.sdds.plasma.giga.app.styles.avatar.M
+import com.sdds.plasma.giga.app.styles.avatar.S
 import com.sdds.plasma.giga.app.styles.avatar.Xxl
 import com.sdds.plasma.giga.app.styles.avatargroup.AvatarGroup
 import com.sdds.plasma.giga.app.styles.avatargroup.S
 import com.sdds.testing.compose.RoborazziConfigCompose
 import com.sdds.testing.compose.avatar.AvatarGroupSizeS
+import com.sdds.testing.compose.avatar.AvatarSizeLBadgeTopEndContentStart
+import com.sdds.testing.compose.avatar.AvatarSizeLCounterTopEnd
 import com.sdds.testing.compose.avatar.AvatarSizeM
+import com.sdds.testing.compose.avatar.AvatarSizeMBadgeBottomStart
+import com.sdds.testing.compose.avatar.AvatarSizeMCounterBottomEnd
+import com.sdds.testing.compose.avatar.AvatarSizeSCounterBottomStart
 import com.sdds.testing.compose.avatar.AvatarSizeXxl
+import com.sdds.testing.compose.avatar.AvatarSizeXxlBadgeBottomEndContentEnd
+import com.sdds.testing.compose.avatar.AvatarSizeXxlBadgeTopStartContentStart
+import com.sdds.testing.compose.avatar.AvatarSizeXxlCounterTopStart
 import com.sdds.testing.vs.SDK_NUMBER
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -43,6 +53,62 @@ class ComposeAvatarScreenshotTest(
     fun testAvatarGroupSizeS() {
         composeTestRule.content {
             AvatarGroupSizeS(style = AvatarGroup.S.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeXxlCounterTopStart() {
+        composeTestRule.content {
+            AvatarSizeXxlCounterTopStart(style = Avatar.Xxl.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeLCounterTopEnd() {
+        composeTestRule.content {
+            AvatarSizeLCounterTopEnd(style = Avatar.L.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeMCounterBottomEnd() {
+        composeTestRule.content {
+            AvatarSizeMCounterBottomEnd(style = Avatar.M.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeSCounterBottomStart() {
+        composeTestRule.content {
+            AvatarSizeSCounterBottomStart(style = Avatar.S.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeXxlBadgeTopStartContentStart() {
+        composeTestRule.content {
+            AvatarSizeXxlBadgeTopStartContentStart(style = Avatar.Xxl.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeXxlBadgeBottomEndContentEnd() {
+        composeTestRule.content {
+            AvatarSizeXxlBadgeBottomEndContentEnd(style = Avatar.Xxl.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeLBadgeTopEndContentStart() {
+        composeTestRule.content {
+            AvatarSizeLBadgeTopEndContentStart(style = Avatar.L.style())
+        }
+    }
+
+    @Test
+    fun testAvatarSizeMBadgeBottomStart() {
+        composeTestRule.content {
+            AvatarSizeMBadgeBottomStart(style = Avatar.M.style())
         }
     }
 }
