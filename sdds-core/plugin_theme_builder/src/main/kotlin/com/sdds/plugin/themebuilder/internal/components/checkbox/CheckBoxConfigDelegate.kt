3cc0ffb8f25a0c04ba28210ce8fd66237a791e4b
Мельников Никита <namelnikov@sberdevices.ru>
2025-04-28 13:49:34 +0300

    feat(sdds-acore/theme-builder): Checkbox and checkBoxGroup generators were implemented

--- sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/components/checkbox/CheckBoxConfigDelegate.kt
+++ sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/components/checkbox/CheckBoxConfigDelegate.kt
@@ -6,6 +6,7 @@ import com.sdds.plugin.themebuilder.internal.components.ComponentConfigDelegate
 import com.sdds.plugin.themebuilder.internal.components.ComponentStyleGenerator
 import com.sdds.plugin.themebuilder.internal.components.StyleGeneratorDependencies
 import com.sdds.plugin.themebuilder.internal.components.base.Component
+import com.sdds.plugin.themebuilder.internal.components.checkbox.view.CheckBoxStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.serializer.Serializer
 import com.sdds.plugin.themebuilder.internal.utils.decode
 import com.sdds.plugin.themebuilder.internal.utils.techToSnakeCase
@@ -19,7 +20,17 @@ internal class CheckBoxConfigDelegate : ComponentConfigDelegate<CheckBoxConfig>(
     override fun createViewGenerator(
         deps: StyleGeneratorDependencies,
         component: Component,
-    ) = null
+    ): ComponentStyleGenerator<CheckBoxConfig> {
+        return CheckBoxStyleGeneratorView(
+            xmlBuilderFactory = deps.xmlBuilderFactory,
+            resourceReferenceProvider = deps.resourceReferenceProvider,
+            dimensAggregator = deps.dimensAggregator,
+            outputResDir = deps.outputResDir,
+            resourcePrefix = deps.resourcePrefixConfig.resourcePrefix,
+            viewColorStateGeneratorFactory = deps.viewColorStateGeneratorFactory,
+            colorStateListGeneratorFactory = deps.colorStateListGeneratorFactory,
+        )
+    }
 
     override fun createComposeGenerator(
         deps: StyleGeneratorDependencies,
