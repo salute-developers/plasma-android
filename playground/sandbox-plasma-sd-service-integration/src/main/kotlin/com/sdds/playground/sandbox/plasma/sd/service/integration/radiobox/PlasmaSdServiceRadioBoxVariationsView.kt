3724159511c467557a05744e49134bebdad77183
Мельников Никита <namelnikov@sberdevices.ru>
2025-05-06 12:11:17 +0300

    feat(sdds-acore/sandbox): Integration modules were update

--- playground/sandbox-plasma-sd-service-integration/src/main/kotlin/com/sdds/playground/sandbox/plasma/sd/service/integration/radiobox/PlasmaSdServiceRadioBoxVariationsView.kt
+++ playground/sandbox-plasma-sd-service-integration/src/main/kotlin/com/sdds/playground/sandbox/plasma/sd/service/integration/radiobox/PlasmaSdServiceRadioBoxVariationsView.kt
@@ -6,6 +6,7 @@ import com.sdds.playground.sandbox.plasma.sd.service.integration.s
 internal object PlasmaSdServiceRadioBoxVariationsView : ViewStyleProvider<String>() {
     override val variations: Map<String, Int> =
         mapOf(
+            "L" to s.Plasma_SdService_ComponentOverlays_RadioBoxL,
             "M" to s.Plasma_SdService_ComponentOverlays_RadioBoxM,
             "S" to s.Plasma_SdService_ComponentOverlays_RadioBoxS,
         )
