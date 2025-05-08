5bf3ace6dde36521d1fda5d88aad911243b020ff
Мельников Никита <namelnikov@sberdevices.ru>
2025-05-08 07:23:56 +0300

    feat(sdds-acore/sandbox): Integration modules were update

--- playground/sandbox-plasma-sd-service-integration/src/main/kotlin/com/sdds/playground/sandbox/plasma/sd/service/integration/switcher/PlasmaSdServiceSwitchVariationsView.kt
+++ playground/sandbox-plasma-sd-service-integration/src/main/kotlin/com/sdds/playground/sandbox/plasma/sd/service/integration/switcher/PlasmaSdServiceSwitchVariationsView.kt
@@ -7,7 +7,10 @@ internal object PlasmaSdServiceSwitchVariationsView : ViewStyleProvider<String>(
     override val variations: Map<String, Int> =
         mapOf(
             "L" to s.Plasma_SdService_ComponentOverlays_SwitchL,
+            "LToggleS" to s.Plasma_SdService_ComponentOverlays_SwitchLToggleS,
             "M" to s.Plasma_SdService_ComponentOverlays_SwitchM,
+            "MToggleS" to s.Plasma_SdService_ComponentOverlays_SwitchMToggleS,
             "S" to s.Plasma_SdService_ComponentOverlays_SwitchS,
+            "SToggleS" to s.Plasma_SdService_ComponentOverlays_SwitchSToggleS,
         )
 }
