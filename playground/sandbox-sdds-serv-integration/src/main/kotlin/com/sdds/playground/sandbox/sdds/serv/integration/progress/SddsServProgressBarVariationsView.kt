19992fcfcea61335fb6c233637a8c6b5e8286cac
Мельников Никита <namelnikov@sberdevices.ru>
2025-05-15 09:16:09 +0300

    feat(sdds-acore/sandbox): Integration modules were update

--- playground/sandbox-sdds-serv-integration/src/main/kotlin/com/sdds/playground/sandbox/sdds/serv/integration/progress/SddsServProgressBarVariationsView.kt
+++ playground/sandbox-sdds-serv-integration/src/main/kotlin/com/sdds/playground/sandbox/sdds/serv/integration/progress/SddsServProgressBarVariationsView.kt
@@ -9,7 +9,7 @@ internal object SddsServProgressBarVariationsView : ViewStyleProvider<String>() 
             "Default" to s.Serv_Sdds_ComponentOverlays_ProgressBarDefault,
             "Secondary" to s.Serv_Sdds_ComponentOverlays_ProgressBarSecondary,
             "Accent" to s.Serv_Sdds_ComponentOverlays_ProgressBarAccent,
-            "GradientAccent" to s.Serv_Sdds_ComponentOverlays_ProgressBarGradientAccent,
+            "GradientAccent" to s.Serv_Sdds_ComponentOverlays_ProgressBarGradient,
             "Positive" to s.Serv_Sdds_ComponentOverlays_ProgressBarPositive,
             "Warning" to s.Serv_Sdds_ComponentOverlays_ProgressBarWarning,
             "Negative" to s.Serv_Sdds_ComponentOverlays_ProgressBarNegative,
