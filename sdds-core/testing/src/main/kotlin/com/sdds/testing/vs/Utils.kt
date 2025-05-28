7501d8d30446dd72b16c64a3238caa0ab73ca4bd
malilex <alex.malishev270896@gmail.com>
2025-05-28 13:16:31 +0300

    fix: popover content title color was fixed.

--- sdds-core/testing/src/main/kotlin/com/sdds/testing/vs/Utils.kt
+++ sdds-core/testing/src/main/kotlin/com/sdds/testing/vs/Utils.kt
@@ -1,18 +1,9 @@
 package com.sdds.testing.vs
 
 import android.content.Context
-import android.content.res.Configuration
-import android.graphics.Color
+import com.sdds.testing.R
+import com.sdds.uikit.colorFromAttr
 
 internal fun Context.getTextColorPrimary(): Int {
-    return if (isDarkMode()) {
-        Color.WHITE
-    } else {
-        Color.BLACK
-    }
-}
-
-internal fun Context.isDarkMode(): Boolean {
-    val nightMode = resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)
-    return nightMode == Configuration.UI_MODE_NIGHT_YES || nightMode == Configuration.UI_MODE_NIGHT_UNDEFINED
+    return colorFromAttr(R.attr.testingTextColorPrimary)
 }
