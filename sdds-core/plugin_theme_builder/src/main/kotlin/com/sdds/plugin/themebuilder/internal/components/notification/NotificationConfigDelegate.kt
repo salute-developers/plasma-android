965f3f2af2ad240d66fbb5d8195b18eb49581fb9
raininforest <s.velesko@yandex.ru>
2025-06-05 11:41:24 +0300

    fix spotless note

--- sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/components/notification/NotificationConfigDelegate.kt
+++ sdds-core/plugin_theme_builder/src/main/kotlin/com/sdds/plugin/themebuilder/internal/components/notification/NotificationConfigDelegate.kt
@@ -5,8 +5,8 @@ import com.sdds.plugin.themebuilder.internal.builder.KtFileBuilder
 import com.sdds.plugin.themebuilder.internal.components.ComponentConfigDelegate
 import com.sdds.plugin.themebuilder.internal.components.StyleGeneratorDependencies
 import com.sdds.plugin.themebuilder.internal.components.base.Component
-import com.sdds.plugin.themebuilder.internal.components.notification.view.NotificationStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.components.notification.compose.NotificationComposeVariationGenerator
+import com.sdds.plugin.themebuilder.internal.components.notification.view.NotificationStyleGeneratorView
 import com.sdds.plugin.themebuilder.internal.serializer.Serializer
 import com.sdds.plugin.themebuilder.internal.utils.decode
 import com.sdds.plugin.themebuilder.internal.utils.techToCamelCase
