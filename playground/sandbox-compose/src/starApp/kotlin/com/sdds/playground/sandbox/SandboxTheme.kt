02fa78ddb183ce3d4d04d44b79aed4afa3583c68
raininforest <s.velesko@yandex.ru>
2025-06-05 10:40:01 +0300

    star ds sandbox was fixed

--- playground/sandbox-compose/src/starApp/kotlin/com/sdds/playground/sandbox/SandboxTheme.kt
+++ playground/sandbox-compose/src/starApp/kotlin/com/sdds/playground/sandbox/SandboxTheme.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import com.sdds.compose.uikit.LocalButtonStyle
 import com.sdds.compose.uikit.interactions.asInteractive
 import com.sdds.compose.uikit.style.style
 import com.sdds.playground.sandbox.core.compose.LocalNavigationViewStyle
@@ -75,7 +74,6 @@ fun SandboxTheme(
         gradients = if (darkTheme) DarkGradients else LightGradients,
         content = {
             CompositionLocalProvider(
-                LocalButtonStyle provides BasicButton.M.style(),
                 LocalSandboxStyle provides SandboxStyle.create(
                     drawerBackgroundColor = colorScheme.surfaceDefaultSolidSecondary,
                     sheetBackgroundColor = colorScheme.surfaceDefaultSolidCard,
