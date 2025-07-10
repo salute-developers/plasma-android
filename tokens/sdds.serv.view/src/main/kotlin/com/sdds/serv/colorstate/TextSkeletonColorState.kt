94b60cb4acabac5a9347cb1eed3989cec8fd5e3d
raininforest <s.velesko@yandex.ru>
2025-07-10 12:34:00 +0300

    feat(sdds-alibs): skeleton loghter variation was added for view

---
+++ tokens/sdds.serv.view/src/main/kotlin/com/sdds/serv/colorstate/TextSkeletonColorState.kt
@@ -0,0 +1,47 @@
+// AUTO-GENERATED. DO NOT MODIFY this file.
+package com.sdds.serv.colorstate
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.`annotation`.Keep
+import com.sdds.serv.R
+import com.sdds.uikit.colorstate.ColorState
+import com.sdds.uikit.colorstate.ColorStateProvider
+import kotlin.Int
+import kotlin.IntArray
+
+/**
+ * Реализация [ColorState] для компонента TextSkeleton
+ */
+public enum class TextSkeletonColorState(
+    public override val attrs: IntArray,
+) : ColorState {
+    DEFAULT(intArrayOf(R.attr.serv_textskeleton_state_default)),
+    LIGHTER(intArrayOf(R.attr.serv_textskeleton_state_lighter)),
+}
+
+/**
+ * Реализация [ColorStateProvider] для TextSkeletonColorState
+ */
+@Keep
+internal class TextSkeletonColorStateProvider : ColorStateProvider {
+    public override fun obtain(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int,
+    ): TextSkeletonColorState? {
+        val typedArray = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.TextSkeleton,
+            defStyleAttr,
+            defStyleRes,
+        )
+        val stateOrdinal: Int = typedArray.getInt(
+            R.styleable.TextSkeleton_serv_textskeletonColors,
+            0,
+        )
+        typedArray.recycle()
+        return TextSkeletonColorState.values().getOrNull(stateOrdinal)
+    }
+}
