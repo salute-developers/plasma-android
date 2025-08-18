50e404a3a2a3549920f251e7ab958f60848e3b54
angirb <bulinoff@gmail.com>
2025-08-18 15:52:13 +0300

    test(sdds-acore/testing): CodeField screenshot tests

---
+++ sdds-core/testing/src/main/kotlin/com/sdds/testing/compose/codefield/CodeFieldTestCases.kt
@@ -0,0 +1,112 @@
+package com.sdds.testing.compose.codefield
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import com.sdds.compose.uikit.CodeField
+import com.sdds.compose.uikit.CodeFieldStyle
+
+/**
+ * Тест-кейсы для компонента [CodeField]
+ */
+
+/**
+ * Тест-кейс с четырехзначным кодом
+ */
+@Composable
+fun CodeFieldCodeLengthFour(style: CodeFieldStyle) {
+    CodeField(
+        modifier = Modifier
+            .testTag("CodeField"),
+        style = style,
+        codeLength = 4,
+        hidden = false,
+        isItemValid = { it != "q" },
+        onCodeComplete = { it == "1234" },
+        caption = "Caption",
+        captionAlignment = Alignment.Start,
+        enabled = true,
+        animationSpec = null,
+    )
+}
+
+/**
+ * Тест-кейс с четырехзначным кодом
+ */
+@Composable
+fun CodeFieldCodeLengthFive(style: CodeFieldStyle) {
+    CodeField(
+        modifier = Modifier
+            .testTag("CodeField"),
+        style = style,
+        codeLength = 5,
+        hidden = false,
+        isItemValid = { it != "q" },
+        onCodeComplete = { it == "12345" },
+        caption = "Caption",
+        captionAlignment = Alignment.CenterHorizontally,
+        enabled = true,
+        animationSpec = null,
+    )
+}
+
+/**
+ * Тест-кейс со скрытым кодом
+ */
+@Composable
+fun CodeFieldCodeHidden(style: CodeFieldStyle) {
+    CodeField(
+        modifier = Modifier
+            .testTag("CodeField"),
+        style = style,
+        codeLength = 4,
+        hidden = true,
+        isItemValid = { it != "q" },
+        onCodeComplete = { it == "1234" },
+        caption = "Caption",
+        captionAlignment = Alignment.CenterHorizontally,
+        enabled = true,
+        animationSpec = null,
+    )
+}
+
+/**
+ * Тест-кейс с шестизначным кодом
+ */
+@Composable
+fun CodeFieldCodeLengthSix(style: CodeFieldStyle) {
+    CodeField(
+        modifier = Modifier
+            .testTag("CodeField"),
+        style = style,
+        codeLength = 6,
+        hidden = false,
+        isItemValid = { it != "q" },
+        onCodeComplete = { it == "123456" },
+        caption = "Caption",
+        captionAlignment = Alignment.CenterHorizontally,
+        enabled = true,
+        animationSpec = null,
+    )
+}
+
+/**
+ * Тест-кейс с шестизначным кодом без Caption
+ */
+@Composable
+fun CodeFieldCodeLengthSixNoCaption(style: CodeFieldStyle) {
+    CodeField(
+        modifier = Modifier
+            .testTag("CodeField"),
+        style = style,
+        codeLength = 6,
+        hidden = false,
+        isItemValid = { it != "q" },
+        onCodeComplete = { it == "123456" },
+        caption = "",
+        captionAlignment = Alignment.CenterHorizontally,
+        enabled = true,
+        animationSpec = null,
+    )
+}
