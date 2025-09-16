16999f8f9b20dd400e430510029e0462892e27ae
angirb <bulinoff@gmail.com>
2025-09-16 15:25:09 +0300

    test(sdds-acore/testing): Added vs notificationContent screenshot tests

---
+++ sdds-core/testing/src/main/kotlin/com/sdds/testing/vs/notificationcontent/NotificationContentTestCases.kt
@@ -0,0 +1,126 @@
+package com.sdds.testing.vs.notificationcontent
+
+import com.sdds.testing.R
+import com.sdds.testing.vs.ComponentScope
+import com.sdds.testing.vs.RoborazziConfig
+
+/**
+ * Тест-кейсы для компонента [NotificationContent]
+ */
+@Suppress("UndocumentedPublicFunction")
+abstract class NotificationContentTestCases(mode: String) : RoborazziConfig(mode) {
+
+    /**
+     * PLASMA-T2245
+     */
+    abstract fun testNotificationContentButtonStretchDefaultTitleTextHasAction()
+
+    /**
+     * PLASMA-T2246
+     */
+    abstract fun testNotificationContentNoTitleButtonStretchIconTopPositive()
+
+    /**
+     * PLASMA-T2247
+     */
+    abstract fun testNotificationContentNoTextButtonStretchIconStartNegative()
+
+    /**
+     * PLASMA-T2248
+     */
+    abstract fun testNotificationContentNoTextTitleButtonStretchIconTopWarning()
+
+    /**
+     * PLASMA-T2249
+     */
+    abstract fun testNotificationContentLongTextButtonStretchIconStartInfo()
+
+    /**
+     * PLASMA-T2250
+     */
+    abstract fun testNotificationContentNoButtonStretchIconStartDefault()
+
+    fun ComponentScope.notificationContentButtonStretchDefaultTitleTextHasAction(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "Title",
+                text = "Text",
+                hasActions = true,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+
+    fun ComponentScope.notificationContentNoTitleButtonStretchIconTopPositive(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "",
+                text = "Text",
+                hasActions = true,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+
+    fun ComponentScope.notificationContentNoTextButtonStretchIconStartNegative(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "Title",
+                text = "",
+                hasActions = true,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+
+    fun ComponentScope.notificationContentNoTextTitleButtonStretchIconTopWarning(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "",
+                text = "",
+                hasActions = true,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+
+    fun ComponentScope.notificationContentLongTextButtonStretchIconStartInfo(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "This is long text for testing purposes",
+                text = "This is long text for testing purposes",
+                hasActions = false,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+
+    fun ComponentScope.notificationContentNoButtonStretchIconStartDefault(style: Int) =
+        notificationContent(
+            style = style,
+            context = context,
+            state = NotificationContentUiState(
+                variant = "",
+                title = "Title",
+                text = "Text",
+                hasActions = true,
+            ),
+        ).apply {
+            id = R.id.notification_content
+        }
+}
---
+++ tokens/plasma.sd.service.view/src/test/kotlin/com/sdds/plasma/sd/service/ViewSystemNotificationContentScreenshotTest.kt
@@ -0,0 +1,72 @@
+package com.sdds.plasma.sd.service
+
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.sdds.testing.vs.SDK_NUMBER
+import com.sdds.testing.vs.notificationcontent.NotificationContentTestCases
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [SDK_NUMBER], qualifiers = RobolectricDeviceQualifiers.Pixel6)
+class ViewSystemNotificationContentScreenshotTest(
+    theme: String,
+) : NotificationContentTestCases(theme) {
+
+    @Test
+    override fun testNotificationContentButtonStretchDefaultTitleTextHasAction() {
+        themedComponent {
+            notificationContentButtonStretchDefaultTitleTextHasAction(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchDefault,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTitleButtonStretchIconTopPositive() {
+        themedComponent {
+            notificationContentNoTitleButtonStretchIconTopPositive(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchIconTopPositive,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTextButtonStretchIconStartNegative() {
+        themedComponent {
+            notificationContentNoTextButtonStretchIconStartNegative(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchIconStartNegative,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTextTitleButtonStretchIconTopWarning() {
+        themedComponent {
+            notificationContentNoTextTitleButtonStretchIconTopWarning(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchIconTopWarning,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentLongTextButtonStretchIconStartInfo() {
+        themedComponent {
+            notificationContentLongTextButtonStretchIconStartInfo(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchIconStartInfo,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoButtonStretchIconStartDefault() {
+        themedComponent {
+            notificationContentNoButtonStretchIconStartDefault(
+                R.style.Plasma_SdService_ComponentOverlays_NotificationContentButtonStretchIconStartDefault,
+            )
+        }
+    }
+}
