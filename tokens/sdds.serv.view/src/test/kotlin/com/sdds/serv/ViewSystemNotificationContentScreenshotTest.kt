c05e3025fc9c009628ca8da9d153286caef5a786
angirb <119584718+angirb@users.noreply.github.com>
2025-09-17 09:18:13 +0300

    test(sdds-acore/testing): Added vs notificationContent screenshot tests (#542)
    * test(sdds-acore/testing): Added vs notificationContent screenshot tests
    
    * test(sdds-acore/testing): Added two new cases to compose and vs notification content tests

---
+++ tokens/sdds.serv.view/src/test/kotlin/com/sdds/serv/ViewSystemNotificationContentScreenshotTest.kt
@@ -0,0 +1,90 @@
+package com.sdds.serv
+
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.sdds.testing.vs.SDK_NUMBER
+import com.sdds.testing.vs.notificationcontent.NotificationContentTestCases
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [SDK_NUMBER], qualifiers = RobolectricDeviceQualifiers.Pixel6)
+class ViewSystemNotificationContentScreenshotTest(
+    theme: String,
+) : NotificationContentTestCases(theme) {
+
+    @Test
+    override fun testNotificationContentButtonStretchDefaultTitleTextHasAction() {
+        themedComponent {
+            notificationContentButtonStretchDefaultTitleTextHasAction(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchDefault,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTitleButtonStretchIconTopPositive() {
+        themedComponent {
+            notificationContentNoTitleButtonStretchIconTopPositive(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchIconTopPositive,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTextButtonStretchIconStartNegative() {
+        themedComponent {
+            notificationContentNoTextButtonStretchIconStartNegative(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchIconStartNegative,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoTextTitleButtonStretchIconTopWarning() {
+        themedComponent {
+            notificationContentNoTextTitleButtonStretchIconTopWarning(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchIconTopWarning,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentLongTextButtonStretchIconStartInfo() {
+        themedComponent {
+            notificationContentLongTextButtonStretchIconStartInfo(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchIconStartInfo,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentNoButtonStretchIconStartDefault() {
+        themedComponent {
+            notificationContentNoButtonStretchIconStartDefault(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchIconStartDefault,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentButtonStretchPositive() {
+        themedComponent {
+            notificationContentButtonStretchDefaultTitleTextHasAction(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchPositive,
+            )
+        }
+    }
+
+    @Test
+    override fun testNotificationContentButtonStretchWarning() {
+        themedComponent {
+            notificationContentButtonStretchDefaultTitleTextHasAction(
+                R.style.Serv_Sdds_ComponentOverlays_NotificationContentButtonStretchWarning,
+            )
+        }
+    }
+}
