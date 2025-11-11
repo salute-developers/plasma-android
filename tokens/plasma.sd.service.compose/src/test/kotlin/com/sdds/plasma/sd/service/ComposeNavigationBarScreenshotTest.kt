c46499069e4cbbc2dddcccdfb585779a8c2672f5
angirb <119584718+angirb@users.noreply.github.com>
2025-11-11 15:36:32 +0300

    PLASMA-5982: test(sdds-acore/testing) NavigationBar screenshot tests (#589)
    * test(sdds-acore/testing) NavigationBar screenshot tests
    
    * fix spotless check

---
+++ tokens/plasma.sd.service.compose/src/test/kotlin/com/sdds/plasma/sd/service/ComposeNavigationBarScreenshotTest.kt
@@ -0,0 +1,73 @@
+package com.sdds.plasma.sd.service
+
+import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.sdds.compose.uikit.style.style
+import com.sdds.plasma.sd.service.styles.navigationbar.HasBackground
+import com.sdds.plasma.sd.service.styles.navigationbar.NavigationBarInternalPage
+import com.sdds.plasma.sd.service.styles.navigationbar.NavigationBarMainPage
+import com.sdds.plasma.sd.service.styles.navigationbar.NoBackground
+import com.sdds.plasma.sd.service.styles.navigationbar.Rounded
+import com.sdds.plasma.sd.service.styles.navigationbar.Shadow
+import com.sdds.testing.compose.RoborazziConfigCompose
+import com.sdds.testing.compose.navigationbar.NavigationBarContentBottomStart
+import com.sdds.testing.compose.navigationbar.NavigationBarLongTextBottomStartInline
+import com.sdds.testing.compose.navigationbar.NavigationBarLongTextContentInlineCenterBottom
+import com.sdds.testing.compose.navigationbar.NavigationBarTextContentBottomCenterBottom
+import com.sdds.testing.compose.navigationbar.NavigationBarTextContentInlineCenter
+import com.sdds.testing.compose.navigationbar.NavigationBarTextHasActionEndInlineEndBottom
+import com.sdds.testing.vs.SDK_NUMBER
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.GraphicsMode
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+@GraphicsMode(GraphicsMode.Mode.NATIVE)
+@Config(sdk = [SDK_NUMBER], qualifiers = RobolectricDeviceQualifiers.Pixel6)
+class ComposeNavigationBarScreenshotTest(
+    theme: String,
+) : RoborazziConfigCompose(theme) {
+
+    @Test
+    fun testNavBarMainPageNoBackGround() {
+        composeTestRule.content {
+            NavigationBarTextContentInlineCenter(NavigationBarMainPage.NoBackground.style())
+        }
+    }
+
+    @Test
+    fun testNavBarNoBackgroundRounded() {
+        composeTestRule.content {
+            NavigationBarContentBottomStart(NavigationBarInternalPage.NoBackground.Rounded.style())
+        }
+    }
+
+    @Test
+    fun testNavBarHasBackground() {
+        composeTestRule.content {
+            NavigationBarLongTextContentInlineCenterBottom(NavigationBarInternalPage.HasBackground.style())
+        }
+    }
+
+    @Test
+    fun testNavBarHasBackgroundRounded() {
+        composeTestRule.content {
+            NavigationBarTextHasActionEndInlineEndBottom(NavigationBarInternalPage.HasBackground.Rounded.style())
+        }
+    }
+
+    @Test
+    fun testNavBarHasBackgroundShadow() {
+        composeTestRule.content {
+            NavigationBarLongTextBottomStartInline(NavigationBarInternalPage.HasBackground.Shadow.style())
+        }
+    }
+
+    @Test
+    fun testNavBarHasBackgroundShadowRounded() {
+        composeTestRule.content {
+            NavigationBarTextContentBottomCenterBottom(NavigationBarInternalPage.HasBackground.Shadow.Rounded.style())
+        }
+    }
+}
