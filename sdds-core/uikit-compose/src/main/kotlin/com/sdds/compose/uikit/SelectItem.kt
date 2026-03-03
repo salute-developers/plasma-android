a2e7678d986d41eb513af82dcba66005d76272fd
raininforest <s.velesko@yandex.ru>
2026-03-03 14:04:19 +0300

    Add SelectItem with slots

--- sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/SelectItem.kt
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/SelectItem.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,7 +24,68 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
-import com.sdds.compose.uikit.internal.common.background
+
+/**
+ * Элемент компонента выбора [Select].
+ * Поддерживает представления одиночного (radio) и множественного (checkbox) выбора.
+ *
+ * Представляет собой строку с элементом управления слева и пользовательским контентом справа.
+ * Внешний вид компонента настраивается через [SelectItemStyle].
+ *
+ * @param checked Флаг состояния выбора элемента. Определяет отображение элемента управления.
+ * @param onClick Обработчик, вызываемый при клике на элемент. Должен изменять состояние [checked].
+ * @param modifier Модификатор для настройки внешнего вида и расположения компонента.
+ * @param style Стиль компонента, определяющий цвета, размеры и форму. По умолчанию используется [LocalSelectItemStyle.current].
+ * @param enabled Флаг доступности компонента. Если `false`, компонент отображается с пониженной прозрачностью и не реагирует на клики.
+ * @param disclosureEnabled Флаг включения [disclosureContent]
+ * @param labelContent контент надписи
+ * @param subtitleContent контент подзаголовка
+ * @param startContent контент в начале
+ * @param endContent контент в конце
+ * @param disclosureContent контент disclosure
+ * @param interactionSource Источник взаимодействий для отслеживания состояний (наведение, нажатие и т.д.).
+ * @param titleContent контент заголовка
+ *
+ * @see SelectItemStyle Стиль компонента выбора
+ * @see SelectItemType Тип выбора (одиночный/множественный)
+ */
+@Composable
+@NonRestartableComposable
+fun SelectItem(
+    checked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    style: SelectItemStyle = LocalSelectItemStyle.current,
+    enabled: Boolean = true,
+    disclosureEnabled: Boolean = true,
+    labelContent: (@Composable () -> Unit)? = null,
+    subtitleContent: (@Composable () -> Unit)? = null,
+    startContent: (@Composable RowScope.() -> Unit)? = null,
+    endContent: (@Composable RowScope.() -> Unit)? = null,
+    disclosureContent: (@Composable RowScope.() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    titleContent: @Composable () -> Unit,
+) {
+    SelectItem(
+        checked = checked,
+        onClick = onClick,
+        modifier = modifier,
+        style = style,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        content = {
+            Cell(
+                style = style.cellStyle,
+                titleContent = titleContent,
+                labelContent = labelContent,
+                subtitleContent = subtitleContent,
+                startContent = startContent,
+                endContent = endContent,
+                disclosureContent = if (disclosureEnabled) disclosureContent else null,
+            )
+        }
+    )
+}
 
 /**
  * Элемент компонента выбора [Select].
@@ -105,7 +168,9 @@ fun SelectItem(
                 controlContent()
             }
         }
-        Box { content() }
+        CompositionLocalProvider(LocalCellStyle provides style.cellStyle) {
+            content()
+        }
     }
 }
 
