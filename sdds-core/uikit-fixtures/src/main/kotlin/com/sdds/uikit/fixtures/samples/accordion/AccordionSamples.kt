d25aa42eddd044dbbaf2afb22ae3107af450ec9f
raininforest <s.velesko@yandex.ru>
2026-03-20 16:42:58 +0300

    fix(sdds-acore/samples): uikit fixtures was fixed

--- sdds-core/uikit-fixtures/src/main/kotlin/com/sdds/uikit/fixtures/samples/accordion/AccordionSamples.kt
+++ sdds-core/uikit-fixtures/src/main/kotlin/com/sdds/uikit/fixtures/samples/accordion/AccordionSamples.kt
@@ -2,12 +2,12 @@ package com.sdds.uikit.fixtures.samples.accordion
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import com.sdds.docs.DocSample
-import com.sdds.uikit.Accordion
 import com.sdds.uikit.fixtures.databinding.SamplesAccordionBinding
 
 @DocSample
-fun Accordion_Simple(context: Context): Accordion {
+fun Accordion_Simple(context: Context): View {
     val binding = SamplesAccordionBinding.inflate(LayoutInflater.from(context))
     return binding.accordionSimple
 }
