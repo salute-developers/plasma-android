90497a7d0e2a84073b39bafdc8330b333a321dd2
SerraMorec <patatyu6@gmail.com>
2026-04-01 15:10:41 +0300

    feat(sdds-acore/uikit): fix test cases imports

--- tokens/sdds.serv.view/src/test/kotlin/com/sdds/serv/ViewSystemSelectScreenshotTest.kt
+++ tokens/sdds.serv.view/src/test/kotlin/com/sdds/serv/ViewSystemSelectScreenshotTest.kt
@@ -9,8 +9,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
 import com.github.takahirom.roborazzi.captureScreenRoboImage
-import com.sdds.testing.vs.SDK_NUMBER
-import com.sdds.testing.vs.select.SelectTestCases
+import com.sdds.uikit.fixtures.SDK_NUMBER
+import com.sdds.uikit.fixtures.testcases.SelectTestCases
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.ParameterizedRobolectricTestRunner
