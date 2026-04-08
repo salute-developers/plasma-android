112b46dbe7577edfce11f2adeeef1327030d4aac
raininforest <s.velesko@yandex.ru>
2026-04-08 17:21:56 +0300

    configCodec rework

--- sdds-core/config-codec/src/main/kotlin/com/sdds/utils/config/codec/internal/CommonConfig.kt
+++ sdds-core/config-codec/src/main/kotlin/com/sdds/utils/config/codec/internal/CommonConfig.kt
@@ -5,32 +5,40 @@ import kotlinx.serialization.json.JsonObject
 
 @Serializable
 internal data class CommonConfig(
-    val rootPropertyId: String = "",
-    val commonValues: JsonObject? = null,
-    val properties: List<ConfigProperty> = emptyList(),
+    val rootVariationId: String = "",
+    val colorSchemeVariationId: String = "",
+    val invariants: JsonObject? = null,
+    val variations: List<Variation> = emptyList(),
+    val defaults: Set<DefaultVariationValue> = emptySet(),
 )
 
 @Serializable
-internal data class ConfigProperty(
+internal data class Variation(
     val id: String,
     val name: String,
-    val values: Set<ConfigPropertyValue>
+    val values: Set<VariationValue>
 )
 
 @Serializable
-internal data class ConfigPropertyValue(
+internal data class VariationValue(
     val name: String,
-    val targets: Set<ConfigPropertyTarget>? = null,
+    val targets: Set<VariationValueTarget>? = null,
     val props: JsonObject? = null,
 )
 
 @Serializable
-internal data class ConfigPropertyTarget(
-    val properties: Set<ConfigPropertyTargetInfo>
+internal data class VariationValueTarget(
+    val properties: Set<TargetInfo>
 )
 
 @Serializable
-internal data class ConfigPropertyTargetInfo(
+internal data class TargetInfo(
+    val id: String,
+    val value: String,
+)
+
+@Serializable
+internal data class DefaultVariationValue(
     val id: String,
     val value: String,
 )
