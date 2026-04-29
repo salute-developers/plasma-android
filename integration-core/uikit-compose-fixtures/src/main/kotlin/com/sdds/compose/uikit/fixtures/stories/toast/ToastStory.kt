af6aaed991e7844a18c28e765c21d8a047861d69
SerraMorec <patatyu6@gmail.com>
2026-04-29 13:57:32 +0300

    feat(sdds-acore/uikit-compose): Toast via modal was improved

--- integration-core/uikit-compose-fixtures/src/main/kotlin/com/sdds/compose/uikit/fixtures/stories/toast/ToastStory.kt
+++ integration-core/uikit-compose-fixtures/src/main/kotlin/com/sdds/compose/uikit/fixtures/stories/toast/ToastStory.kt
@@ -2,15 +2,25 @@ package com.sdds.compose.uikit.fixtures.stories.toast
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import com.sdds.compose.sandbox.ComposeBaseStory
 import com.sdds.compose.uikit.Button
 import com.sdds.compose.uikit.Icon
+import com.sdds.compose.uikit.Modal
+import com.sdds.compose.uikit.ModalGravity
 import com.sdds.compose.uikit.Text
 import com.sdds.compose.uikit.Toast
 import com.sdds.compose.uikit.ToastStyle
@@ -35,6 +45,8 @@ data class ToastUiState(
     val hasContentEnd: Boolean = true,
     val position: OverlayPosition = OverlayPosition.BottomCenter,
     val autoDismiss: Boolean = true,
+    val showViaModal: Boolean = false,
+    val modalGravity: ModalGravity = ModalGravity.Center,
 ) : UiState {
 
     override fun updateVariant(appearance: String, variant: String): UiState {
@@ -55,27 +67,35 @@ object ToastStory : ComposeBaseStory<ToastUiState, ToastStyle>(
         state: ToastUiState,
     ) {
         val overlayManager = LocalOverlayManager.current
-        Button(
-            modifier = Modifier.align(Alignment.Center),
-            label = "show",
-            onClick = {
-                overlayManager.showToast(
-                    position = state.position,
-                    durationMillis = OverlayManager.OVERLAY_DURATION_SLOW_MILLIS
-                        .takeIf { state.autoDismiss },
-                ) {
-                    Toast(
-                        style = style,
-                        contentStart = getContentStart(state.hasContentStart),
-                        contentEnd = getContentEnd(state.hasContentEnd) {
-                            overlayManager.remove(it)
-                        },
+        if (state.showViaModal) {
+            ToastViaModal(
+                state = state,
+                style = style,
+                overlayManager = overlayManager,
+            )
+        } else {
+            Button(
+                modifier = Modifier.align(Alignment.Center),
+                label = "show",
+                onClick = {
+                    overlayManager.showToast(
+                        position = state.position,
+                        durationMillis = OverlayManager.OVERLAY_DURATION_SLOW_MILLIS
+                            .takeIf { state.autoDismiss },
                     ) {
-                        Text(state.text)
+                        Toast(
+                            style = style,
+                            contentStart = getContentStart(state.hasContentStart),
+                            contentEnd = getContentEnd(state.hasContentEnd) {
+                                overlayManager.remove(it)
+                            },
+                        ) {
+                            Text(state.text)
+                        }
                     }
-                }
-            },
-        )
+                },
+            )
+        }
     }
 
     @Composable
@@ -110,6 +130,68 @@ object ToastStory : ComposeBaseStory<ToastUiState, ToastStyle>(
                 }
             },
         )
+    }
+}
+
+@Composable
+private fun BoxScope.ToastViaModal(
+    state: ToastUiState,
+    style: ToastStyle,
+    overlayManager: OverlayManager,
+) {
+    var showDialog by remember { mutableStateOf(false) }
+    if (state.showViaModal) {
+        Button(
+            modifier = Modifier.align(Alignment.Center),
+            label = "show dialog",
+            onClick = { showDialog = true },
+        )
+        if (showDialog) {
+            Modal(
+                show = true,
+                onDismissRequest = { showDialog = false },
+                modifier = Modifier.width(300.dp),
+                gravity = state.modalGravity,
+                dialogProperties = DialogProperties(
+                    dismissOnBackPress = false,
+                    dismissOnClickOutside = false,
+                ),
+                hasClose = true,
+                edgeToEdge = true,
+                dimBackground = false,
+                useNativeBlackout = false,
+                closeIcon = painterResource(R.drawable.ic_close_24),
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Button(
+                        modifier = Modifier.align(Alignment.Center),
+                        label = "show toast",
+                        onClick = {
+                            overlayManager.showToast(
+                                onDismiss = {},
+                                position = state.position,
+                                durationMillis = OverlayManager.OVERLAY_DURATION_SLOW_MILLIS
+                                    .takeIf { state.autoDismiss },
+                            ) {
+                                Toast(
+                                    style = style,
+                                    contentStart = getContentStart(state.hasContentStart),
+                                    contentEnd = getContentEnd(state.hasContentEnd) {
+                                        overlayManager.remove(it)
+                                    },
+                                ) {
+                                    Text(state.text)
+                                }
+                            }
+                        },
+                    )
+                }
+            }
+        }
     }
 }
 
