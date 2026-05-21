6d9cb4ee42e68aa7332bad3a8833232f55ea6990
malilex <alex.malishev270896@gmail.com>
2026-05-21 12:40:25 +0300

    fix(sdds-acore/uikit-compose): Icon now supports Color.Unsecified.

--- sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/graphics/brush/BrushProducer.kt
+++ sdds-core/uikit-compose/src/main/kotlin/com/sdds/compose/uikit/graphics/brush/BrushProducer.kt
@@ -1,6 +1,9 @@
 package com.sdds.compose.uikit.graphics.brush
 
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.isUnspecified
 
 /**
  * Поставщик кисти.
@@ -14,3 +17,21 @@ fun interface BrushProducer {
      */
     operator fun invoke(): Brush
 }
+
+/**
+ * Специальный экземпляр, возвращающий неопределённую (неустановленную) кисть
+ */
+val Brush.Companion.Unspecified: Brush get() = UnspecifiedBrush
+
+/**
+ * Проверяет является ли эта кисть неопределеной
+ */
+val Brush.isUnspecified: Boolean
+    get() = (this == UnspecifiedBrush) || (this as? SolidColor)?.value?.isUnspecified == true
+
+/**
+ * Проверяет является ли эта кисть определенной
+ */
+val Brush.isSpecified: Boolean get() = !isUnspecified
+
+private val UnspecifiedBrush = SolidColor(Color.Unspecified)
