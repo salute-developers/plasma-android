37d9a12c487ca8756cd4494c4848a0b66cb1bcd3
Nikita Melnikov <162089237+SerraMorec@users.noreply.github.com>
2026-09-15 14:58:33 +0300

    feat(sdds-alibs/plasma-homeds-compose): ProductCard was implemented (#914)

--- tokens/plasma.homeds.compose/integration/src/main/kotlin/com/sdds/plasma/homeds/integration/custom/PlasmaHomedsComposeCustomComponents.kt
+++ tokens/plasma.homeds.compose/integration/src/main/kotlin/com/sdds/plasma/homeds/integration/custom/PlasmaHomedsComposeCustomComponents.kt
@@ -1,19 +1,23 @@
 package com.sdds.plasma.homeds.integration.custom
 
 import com.sdds.sandbox.Component
-import com.sdds.sandbox.ComponentGroup
 import com.sdds.sandbox.ComponentKey
 import com.sdds.sandbox.ComponentProvider
 
 /**
  *  Ключ для компонента NumberPanel
  */
-val NumberPanelKey = ComponentKey("NumberPanel", ComponentGroup("Data Entry", 1))
+val NumberPanelKey = ComponentKey("NumberPanel", ComponentKey.BasicButton.group)
 
 /**
  *  Ключ для компонента Bubble
  */
 val BubbleKey = ComponentKey("Bubble", ComponentKey.Overlay.group)
+
+/**
+ *  Ключ для компонента ProductCard
+ */
+val ProductCardKey = ComponentKey("ProductCard", ComponentKey.Card.group)
 
 /**
  *  Компоненты
@@ -32,6 +36,12 @@ object PlasmaHomedsComposeCustomComponents : ComponentProvider() {
                 BubbleKey,
                 mapOf(
                     "Bubble" to PlasmaHomedsBubbleVariationsCompose,
+                ),
+            ),
+            Component(
+                ProductCardKey,
+                mapOf(
+                    "ProductCard" to PlasmaHomedsProductCardVariationsCompose,
                 ),
             ),
         ).associateBy { it.key }
